ff25681867a6fe5de8d63c19ca596a4399187715
Surya_Teja-ITT <vellaturi.d@intimetec.com>
2022-02-22 14:54:08 +0530

    First commit

---
+++ KDataTest/KDataTest.m
@@ -0,0 +1,12 @@
+//
+//  KDataTest.m
+//  KDataTest
+//
+//  Created by Vellaturi Devi Surya Teja on 10/02/22.
+//
+
+#import "KDataTest.h"
+
+@implementation KDataTest
+
+@end
---
+++ build/simulators/include/KDataTest/KDataTest.h
@@ -0,0 +1,12 @@
+//
+//  KDataTest.h
+//  KDataTest
+//
+//  Created by Vellaturi Devi Surya Teja on 10/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KDataTest : NSObject
+
+@end
